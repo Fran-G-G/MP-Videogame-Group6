77de0a15b70872f9f518293b92779c5559dca829
Marcos Vidal Castillo <marcosvicas2006@gmail.com>
2026-04-24 18:27:25 +0200

    te sale ya FRANCISCO?

--- src/CombatMediator.java
+++ src/CombatMediator.java
@@ -344,7 +344,7 @@ public class CombatMediator implements Mediator {
     // =================================================================
     //                INTRO WITH CROSSED SWORDS
     // =================================================================
-
+c
     private void printIntro() {
         System.out.println();
         String[] introArt = {
